001c003de3e84fd2e0f6bf3b8d12a66854a436a0
bitbyrubayat <mdrubayatalam@gmail.com>
2026-09-07 17:14:19 +0600

    Question 2 has been Solved

--- answer.js
+++ answer.js
@@ -1,13 +1,53 @@
 // Question no-1
 
-function describeValue(value){
+// function describeValue(value){
      
-     if(value)
-        return console.log(`"${typeof(value)} | truthy"`);
-    else
-        return console.log(`"${typeof(value)} | falsy"`);
+//      if(value)
+//         return console.log(`"${typeof(value)} | truthy"`);
+//     else
+//         return console.log(`"${typeof(value)} | falsy"`);
 
 
-}
+// }
 
-describeValue(undefined)
+// describeValue(NaN)
+
+// Question no-2
+
+// function getDayType(day = "MONDAY"){
+//     let WorkingDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+//     let WeekendDays = ["Friday","Saturday"];
+//     let capitalized = day.charAt(0).toUpperCase() + day.slice(1); 
+//     let lowerCase = day.charAt(0) + day.slice(1).toLowerCase();
+//     // return lowerCase;
+
+//           switch (true) {
+//             case WeekendDays.includes(capitalized) || WeekendDays.includes(lowerCase):
+//                console.log("Weekend");
+//                 break;
+//             case WeekendDays.includes(lowerCase):
+//                console.log("Weekend");
+//                 break;
+
+//             case WorkingDays.includes(lowerCase):
+//                console.log("Working Days");
+//                 break;
+             
+//             case WorkingDays.includes(capitalized):
+//                console.log("Working Days");
+//                 break;
+                
+                
+//             default:
+//                console.log("invalid day");
+//                 break;
+//           }
+
+// }
+
+
+// getDayType("sunday")
+
+
+
+
